7ba1340b035db10a8192376fc61dec89342ad95a
Saad Abdul Majid <saadmajidnadeem1@gmail.com>
2022-08-08 13:27:28 +0500

    Introduce syslog_ex for logging to the system logger
    Signed-off-by: Saad Abdul Majid <saadmajidnadeem1@gmail.com>

--- example.c
+++ example.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    slog_init(); //optional
     slog_debug("This is very %s. But I would rather want it to be %s%c %d out of %d to be exact%c", "fine", "amazing", '.', 100, 100, '!');
     return 0;
 }
--- stupid_logger.h
+++ stupid_logger.h
@@ -6,30 +6,47 @@
 #include <string.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <syslog.h>
+
+static void syslog_ex(const int priority, const char* format, ...)
+{
+    va_list arglist = {0};
+
+    openlog("SLOGGER", LOG_PID, LOG_USER);
+    va_start(arglist, format);
+    vsyslog(priority, format, arglist);
+    va_end(arglist);
+    closelog();
+}
 
 static void slog_format(const char* tag, const char* format, va_list arglist)
 {
     time_t now = {0};
     const char* now_str = NULL;
     char* _format = NULL;
-    const size_t tag_length = strlen(tag);
-    const size_t format_length = strlen(format);
+    size_t _format_length = 0;
 
     time(&now);
     now_str = ctime(&now);
 
     // timestamp + 1B (space character) + tag + 1B (space character) +
     // format (in argument) + 1B (end of line character) + 1B (null character)
-    _format = (char*)malloc((strlen(now_str) - 1) + 1 + strlen(tag) + 1 + strlen(format) + 1 + 1);
+    _format_length = (strlen(now_str) - 1) + 1 + strlen(tag) + 1 + strlen(format) + 1 + 1;
+    _format = (char*)malloc(_format_length);
     if (!_format)
     {
-        // TODO: syslog here
+        syslog_ex(LOG_ERR, "failed to allocate memory for format string [%u bytes]", _format_length);
         return;
     }
     sprintf(_format, "%.*s %s %s\n", (int)(strlen(now_str) - 1), now_str, tag, format);
 
     vprintf(_format, arglist);
     free(_format);
+}
+
+void slog_init()
+{
+    syslog_ex(LOG_INFO, "logger initialized");
 }
 
 void slog_debug(const char* format, ...)
